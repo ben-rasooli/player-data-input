b68b6b794d6df0779c24a72f3478547d2e43ecdd
Behnam Rasooli <ben.rasooli@gmail.com>
2019-11-05 18:24:17 +1100

    Add DataStorage implementation

--- Assets/Scripts/DataStorage.cs
+++ Assets/Scripts/DataStorage.cs
@@ -1,9 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
 namespace PlayerDataInput
 {
    public class DataStorage
    {
+      string _savePath = Application.persistentDataPath + "/playerData.data";
+      BinaryFormatter _formatter = new BinaryFormatter();
+
       /// <summary>
       /// It appends a player data to the list of player data which was saved previously.
       /// If no list is available, a new one is created.
@@ -11,7 +19,37 @@ namespace PlayerDataInput
       /// <param name="playerData"></param>
       public void Save(PlayerData playerData)
       {
+         List<PlayerData> playersData = File.Exists(_savePath) ? Load() : new List<PlayerData>();
 
+         playersData.Add(playerData);
+
+         FileStream fileStream = new FileStream(_savePath, FileMode.Create);
+         _formatter.Serialize(fileStream, playersData);
+         fileStream.Close();
+      }
+
+      /// <summary>
+      /// It updates an existing player data. Two players are the same if they have same name.
+      /// It throws exeption if the player data doesn't exist.
+      /// </summary>
+      /// <param name="playerData"></param>
+      public void Update(PlayerData playerData)
+      {
+         if (!File.Exists(_savePath))
+            throw new InvalidOperationException("playersData file not found");
+
+         List<PlayerData> playersData = Load();
+
+         if (!playersData.Any(pd => pd.Details[0].Value == playerData.Details[0].Value))
+            throw new InvalidOperationException("specified playerData not found");
+
+         for (int i = 0; i < playersData.Count; i++)
+            if (playersData[i].Details[0].Value == playerData.Details[0].Value)
+               playersData[i] = playerData;
+
+         FileStream fileStream = new FileStream(_savePath, FileMode.Create);
+         _formatter.Serialize(fileStream, playersData);
+         fileStream.Close();
       }
 
       /// <summary>
@@ -21,7 +59,16 @@ namespace PlayerDataInput
       /// <returns></returns>
       public List<PlayerData> Load()
       {
-         return null;
+         List<PlayerData> result;
+
+         if (!File.Exists(_savePath))
+            return new List<PlayerData>();
+
+         FileStream fileStream = new FileStream(_savePath, FileMode.Open);
+         result = _formatter.Deserialize(fileStream) as List<PlayerData>;
+         fileStream.Close();
+
+         return result;
       }
    }
 }
